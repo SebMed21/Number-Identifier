17d3b616b23879f9198f38158b2b1d82d7a33b7b
Sebastian Medina <sebastian.medina301426@gmail.com>
2024-10-16 20:20:16 +0800

    modified:   number-identifier-main.py

--- number-identifier-main.py
+++ number-identifier-main.py
@@ -1,7 +1,21 @@
 
 #define function
     # compare the variables using relational operators
+
+def identifier(num1, num2, num3, num4, num5):
+
+        if num1 > num2:
+            if num1 > num3:
+                if num1 > num4:
+                    if num1 > num5:
+                        print (num1)
+        else:
+             print (num2)
+
+
+
 #input values of the variables
 #call the defined function
+identifier(10,9,8,7,6)
 #display the values
 
