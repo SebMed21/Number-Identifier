890507e0cec0b1d71988b75454d487feebf1796f
Sebastian Medina <sebastian.medina301426@gmail.com>
2024-10-17 11:48:20 +0800

    updated main

--- number-identifier-main.py
+++ number-identifier-main.py
@@ -4,24 +4,16 @@
 
 def identifier(num1, num2, num3, num4, num5):
 
-        if num1 > num2:
-            if num1 > num3:
-                if num1 > num4:
-                    if num1 > num5:
-                        print (num1)
-            else:
-                print (num5)
-
-        else:
-            if num2 > num1:
-                if num2 > num3:
-                    if num2 > num4:
-                        if num2 > num5:
-                            print (num2)
-            else: 
-                print (num5)
-                
+        if num1 > num2 and num1> num3 and num1 > num4 and num1 > num5:
+            print(num1)
+        elif num2 > num3 and num2 > num4 and num2 > num5:
+            print (num2)
+        elif num3 > num4 and num3 > num5:
+            print (num3)
+        elif num4 > num5:
+            print (num4)
         else: 
+            print (num5)
             
 
                 
